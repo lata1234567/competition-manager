2171b83e0708429275fb2cd50054cf7dc35baa90
lata1234567 <wojtek.tatarczyk@wp.pl>
2025-04-28 21:01:55 +0200

    first commit

---
+++ src/main/java/pl/tatarczyk/wojciech/competition_manager/CompetitionManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.tatarczyk.wojciech.competition_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionManagerApplication.class, args);
+	}
+
+}
