03a0c93d3b78ec2beb3498a8097f92fbbff220c5
lata1234567 <wojtek.tatarczyk@wp.pl>
2025-04-30 14:19:02 +0200

    add missing code

--- src/main/java/pl/tatarczyk/wojciech/competition_manager/repository/entity/ApplicationEntity.java
+++ src/main/java/pl/tatarczyk/wojciech/competition_manager/repository/entity/ApplicationEntity.java
@@ -19,4 +19,76 @@ public class ApplicationEntity {
 
     private UserEntity createdBy;
     private LocalDate createdDate;
+
+    public ApplicationEntity() {
+    }
+
+    public ApplicationEntity(Long id, CompetitionEntity competition, UserEntity submitted, UserEntity submittedBy, UserEntity createdBy, LocalDate createdDate) {
+        this.id = id;
+        this.competition = competition;
+        this.submitted = submitted;
+        this.submittedBy = submittedBy;
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CompetitionEntity getCompetition() {
+        return competition;
+    }
+
+    public void setCompetition(CompetitionEntity competition) {
+        this.competition = competition;
+    }
+
+    public UserEntity getSubmitted() {
+        return submitted;
+    }
+
+    public void setSubmitted(UserEntity submitted) {
+        this.submitted = submitted;
+    }
+
+    public UserEntity getSubmittedBy() {
+        return submittedBy;
+    }
+
+    public void setSubmittedBy(UserEntity submittedBy) {
+        this.submittedBy = submittedBy;
+    }
+
+    public UserEntity getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UserEntity createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationEntity{" +
+                "id=" + id +
+                ", competition=" + competition +
+                ", submitted=" + submitted +
+                ", submittedBy=" + submittedBy +
+                ", createdBy=" + createdBy +
+                ", createdDate=" + createdDate +
+                '}';
+    }
 }
