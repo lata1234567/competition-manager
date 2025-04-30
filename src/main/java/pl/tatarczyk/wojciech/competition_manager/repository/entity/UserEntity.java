96d222eebb979341572ada8d7e5ca6ea09bb3630
lata1234567 <wojtek.tatarczyk@wp.pl>
2025-04-30 13:34:08 +0200

    add 2 entities

---
+++ src/main/java/pl/tatarczyk/wojciech/competition_manager/repository/entity/UserEntity.java
@@ -0,0 +1,128 @@
+package pl.tatarczyk.wojciech.competition_manager.repository.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<RoleEntity> roles = new HashSet<>();
+
+    private UserEntity createdBy;
+    private LocalDate createdDate;
+
+    private UserEntity modifyBy;
+    private UserEntity modifyDate;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Set<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleEntity> roles) {
+        this.roles = roles;
+    }
+
+    public UserEntity getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UserEntity createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public UserEntity getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(UserEntity modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public UserEntity getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(UserEntity modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                ", createdBy=" + createdBy +
+                ", createdDate=" + createdDate +
+                ", modifyBy=" + modifyBy +
+                ", modifyDate=" + modifyDate +
+                '}';
+    }
+}
